ddeba41fe892af1828dda7a0be2637f0957e9860
faramarz <37065807+newlighty@users.noreply.github.com>
2021-11-21 00:35:28 +0330

    Revert "some translate changes"
    This reverts commit f8769b19d488240674661fdfcffa725240f8ef13.

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,15 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 
 urlpatterns = [
     path('', views.store, name='store'),
-    path('category/<slug:category_slug>/', views.store, name='products_by_category'),
+    # path('category/<slug:category_slug>/', views.store, name='products_by_category'),
+    # re_path(r'detail/(?P<slug>[-\w]+)/', views.detail),
+    #  re_path(r'(?P<slug>[-\w]+)/', views.detail),
+    re_path(r'v(?P<category_slug>[-\w]+)/', views.store, name='products_by_category'),
+
     path('category/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
     path('search/', views.search, name='search'),
     path('submit_review/<int:product_id>/', views.submit_review, name='submit_review'),
