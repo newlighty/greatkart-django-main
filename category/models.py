f87493d663a57965060c789124421055e7b09a23
faramarz <37065807+newlighty@users.noreply.github.com>
2021-11-21 17:42:54 +0330

    edit admin category language

--- category/models.py
+++ category/models.py
@@ -5,13 +5,14 @@ from django.urls import reverse
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, allow_unicode=True)
+  
     description = models.TextField(max_length=255, blank=True)
     cat_image = models.ImageField(upload_to='photos/categories', blank=True)
 
     class Meta:
-        verbose_name = 'category'
-        verbose_name_plural = 'categories'
+        verbose_name = 'دسته بندی'
+        verbose_name_plural = 'دسته بندی ها'
 
     def get_url(self):
             return reverse('products_by_category', args=[self.slug])
